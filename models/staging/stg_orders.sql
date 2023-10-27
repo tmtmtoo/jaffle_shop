18f12a7d6cac2ed9ce7d3d5f470c80ef6a6b02c1
matsu <tmtmt2209@gmail.com>
2023-10-27 08:58:15 +0000

    fmt: files

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,23 +1,19 @@
-with source as (
+with
+    source as (
 
-    {#-
+        {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_orders') }}
+        select * from {{ ref("raw_orders") }}
 
-),
+    ),
 
-renamed as (
+    renamed as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+        select id as order_id, user_id as customer_id, order_date, status from source
 
-    from source
+    )
 
-)
-
-select * from renamed
+select *
+from renamed
